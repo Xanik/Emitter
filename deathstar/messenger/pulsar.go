3435078468d228a5aa9717208407d4e40831b627
Xanik <sandraike57@gmail.com>
2020-07-03 08:36:56 +0100

    Added Pulsar for deathstar + Config Files

---
+++ deathstar/messenger/pulsar.go
@@ -0,0 +1,61 @@
+package messenger
+
+import (
+	"context"
+	"emitter/deathstar/proto/deathstar_pb"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/apache/pulsar-client-go/pulsar"
+)
+
+// Connect instantiate Pulsar a client
+func Connect(url string) (pulsar.Client, error) {
+	log.Printf("\nCreating new Pulsar connection")
+
+	client, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL:               url,
+		OperationTimeout:  30 * time.Second,
+		ConnectionTimeout: 30 * time.Second,
+	})
+	if err != nil {
+		log.Fatalf("Could not instantiate Pulsar client: %v", err)
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// Consumer consumes messages from pulsar and store in db
+func Consumer(client pulsar.Client, topic string) (deathstar_pb.EventMessage, error) {
+	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+		Topic:            topic,
+		SubscriptionName: "my-sub",
+		Type:             pulsar.Shared,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var event deathstar_pb.EventMessage
+
+	msg, err := consumer.Receive(context.Background())
+	if err != nil {
+		log.Fatal(err)
+		return deathstar_pb.EventMessage{}, err
+	}
+
+	fmt.Printf("Received message msgId: %#v -- content: '%s'\n",
+		msg.ID(), string(msg.Payload()))
+
+	consumer.Ack(msg)
+
+	err = json.Unmarshal(msg.Payload(), &event)
+	if err != nil {
+		return deathstar_pb.EventMessage{}, err
+	}
+	fmt.Printf("Event Unmarshalled Here", event)
+
+	return event, nil
+}
